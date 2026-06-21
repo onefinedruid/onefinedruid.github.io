377e6325481fd9d5d62d6463a340182ff5d73881
Ian Weber <ianweber30@icloud.com>
2026-06-20 21:52:36 -0400

    new export

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1782004399|5320258505';
+const CACHE_VERSION = '1782006730|7651376678';
 /** @type {string} */
 const CACHE_PREFIX = 'SteveShack-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
